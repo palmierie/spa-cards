ccb3efb00de346ceb4e7d007c404b462b020ec9a
palmierie <ericmpalmieri@gmail.com>
2017-08-08 13:21:52 -0500

    finished exercise

--- cards.js
+++ cards.js
@@ -1,1 +1,35 @@
 console.log('cards.js');
+
+var input = document.getElementById("inputText");
+var create = document.getElementById("createBtn");
+var container = document.getElementById("container");
+var cardsArray = [];
+var card ;
+
+function deleteCard(delCard) {
+  let parent = delCard.parentNode;
+  parent.removeChild(delCard);
+}
+
+create.addEventListener("click", function() {
+  
+  card = document.createElement("article");
+  card.classList.add("cardClass");
+  card.innerHTML= `
+    <h2>${input.value}</h2>
+    <input type="button" value="Delete" class="delete-btn">
+    `;
+
+  container.appendChild(card);
+  input.value = '';
+
+  var delBtn = document.getElementsByClassName("delete-btn");
+  cardsArray.push(card);
+    
+});
+
+container.addEventListener("click",()=>{
+  if (event.target.classList == "delete-btn") {
+    deleteCard(this.event.target.parentNode);
+  }
+});
